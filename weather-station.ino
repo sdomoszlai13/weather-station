366506a7991e5b7bc6e392e010d2a6ca45cf4a13
sdomoszlai13 <sebastian.domoszlai@protonmail.com>
2023-03-15 17:07:45 +0100

    Setup done for test display and sensor.

--- weather-station.ino
+++ weather-station.ino
@@ -3,9 +3,28 @@
 #include <Adafruit_SSD1306.h>
 #include <SparkFun_Qwiic_Humidity_AHT20.h>
 
+
+#define SCREEN_WIDTH 128 // OLED display width (in pixels)
+#define SCREEN_HEIGHT 64 // OLED display height (in pixels)
+#define OLED_RESET -1 // Reset pin number (-1: sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C // Display address; 0x3D for 128 X 64, 0x3C for 128 X 32
+
+
 void setup()
 {
-    pinMode(13, OUTPUT);
+    Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+    AHT20 humidity_sensor;
+
+    Serial.begin(115200);
+
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;); // Loop forever
+    }
+
+    display.clearDisplay(); // Clear buffer
+    Wire.begin();
+    humidity_sensor.begin();
 }
 
 void loop()
