a7083bc1177c7357349b8ced46acebe4f2fb7603
sdomoszlai13 <sebastian.domoszlai@protonmail.com>
2023-03-16 22:12:56 +0100

    Added receive() function and setup of transceiver.

--- weather-station.ino
+++ weather-station.ino
@@ -3,6 +3,7 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 #include <SparkFun_Qwiic_Humidity_AHT20.h>
+#include <RF24.h>
 
 
 #define SCREEN_WIDTH 128 // OLED display width (in pixels)
@@ -10,8 +11,14 @@
 #define OLED_RESET -1 // Reset pin number (-1: sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C // Display address; 0x3D for 128 X 64, 0x3C for 128 X 32
 
+// Instantiate a sensor
 AHT20 humidity_sensor;
+
+// Instantiate a display
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Instantiate an nRF24L01 transceiver
+RF24 radio(7, 8);  // Using pin 7 as the CE pin, and pin 8 as the CSN pin
 
 void setup()
 {
@@ -36,14 +43,14 @@ void loop()
 }
 
 
-void printData(int humidity, float temp) {
+void printData(int hum, float temp) {
     display.clearDisplay();
     display.setTextSize(2);
     display.setTextColor(SSD1306_WHITE);    // Draw white text
     display.setCursor(0,0);    // Start at top left corner
 
     display.print("Relative humidity: ");
-    display.print(humidity);
+    display.print(hum);
     display.print("%");
     display.print("\n");
     display.print("Temperature: ");
@@ -51,4 +58,11 @@ void printData(int humidity, float temp) {
 
     display.display();
     delay(400);
+}
+
+
+void receive(float temp, int hum, int pres){
+    // temp = 
+    // hum = 
+    // pres =
 }
