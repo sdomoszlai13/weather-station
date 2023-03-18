8fe5549ab22d779977468b4903293b28db7c634e
sdomoszlai13 <sebastian.domoszlai@protonmail.com>
2023-03-18 13:45:19 +0100

    Added functionality for 16 X 2 display.

--- weather-station.ino
+++ weather-station.ino
@@ -1,12 +1,11 @@
 #include <Arduino.h>
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 #include <SparkFun_Qwiic_Humidity_AHT20.h>
 #include <Adafruit_BMP280.h>
-
+#include <LiquidCrystal_I2C.h>
 #include <RF24.h>
-
+#include <string.h>
 
 #define SCREEN_WIDTH 128    // OLED display width (in pixels)
 #define SCREEN_HEIGHT 64    // OLED display height (in pixels)
@@ -22,7 +21,10 @@ Adafruit_Sensor *bmp_temp = bmp280.getTemperatureSensor();
 Adafruit_Sensor *bmp_pressure = bmp280.getPressureSensor();
 
 // Instantiate a display
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Instantiate a display
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 // Instantiate an nRF24L01 transceiver
 RF24 radio(7, 8);   // Using pin 7 as the CE pin, and pin 8 as the CSN pin
@@ -30,29 +32,23 @@ RF24 radio(7, 8);   // Using pin 7 as the CE pin, and pin 8 as the CSN pin
 void setup(){
 
     Serial.begin(115200);
-/*
+
+    /*
     if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
         Serial.println(F("SSD1306 allocation failed"));
         for(;;);    // Loop forever
     }
 
-
     display.clearDisplay();    // Clear buffer
-*/
+    */
+
     Wire.begin();
     aht20.begin();
 
-    unsigned status;
-    status = bmp280.begin();
-    if (!status) {
-        Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                        "try a different address!"));
-        Serial.print("SensorID was: 0x"); Serial.println(bmp280.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(100);
+    if (!bmp280.begin()){
+        Serial.println("Could not find a valid BMP280 sensor, check wiring"
+                        " or try a different address!");
+        while (1) delay(500);
         Serial.println("Setup finished");
     }
 
@@ -62,29 +58,38 @@ void setup(){
                     Adafruit_BMP280::SAMPLING_X16,  // Pressure oversampling
                     Adafruit_BMP280::FILTER_X16,    // Filtering
                     Adafruit_BMP280::STANDBY_MS_500);   // Standby time
+    
+    lcd.init();
+    lcd.backlight();
 }
 
 
 void loop(){
 
+    // AHT20 measurements
+    float aht20hum = aht20.getHumidity();
+    float aht20temp = aht20.getTemperature();
+
+    // BMP280 measurements
+    float bmp280temp = bmp280.readTemperature();
+    float bmp280pres = bmp280.readPressure();
+
+    delay(50);
+
+    // Print values on display
+    lcd.setCursor(1,0);
+    lcd.print("Temp: ");
+    lcd.setCursor(7,0);
+    lcd.print(bmp280temp);
+    lcd.setCursor(1,1);
+    lcd.print("Pres: ");
+    lcd.setCursor(7,1);
+    bmp280pres = bmp280pres / 100;
+    lcd.print(bmp280pres);
     delay(1000);
-
-    // AHT20
-    Serial.print("Humidity: ");
-    Serial.println(aht20.getHumidity());
-    Serial.print("Temperature: ");
-    Serial.println(aht20.getTemperature());
-
-    // BMP280
-    Serial.print("Temperature: ");
-    Serial.println(bmp280.readTemperature());
-    Serial.print("Pressure: ");
-    Serial.println(bmp280.readPressure());
-
-    delay(300);
 }
 
-
+/*
 void printData(int hum, float temp) {
     display.clearDisplay();
     display.setTextSize(2);
@@ -101,6 +106,7 @@ void printData(int hum, float temp) {
     display.display();
     delay(400);
 }
+*/
 
 
 void receive(float temp, int hum, int pres){
