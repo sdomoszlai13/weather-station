91635d592ccf305b84bcb52956c44e578b9dd011
sdomoszlai13 <sebastian.domoszlai@protonmail.com>
2023-03-18 23:12:00 +0100

    Removed unused code.

--- base_station.ino
+++ base_station.ino
@@ -7,10 +7,6 @@
 #include <RF24.h>
 #include <string.h>
 
-#define SCREEN_WIDTH 128    // OLED display width (in pixels)
-#define SCREEN_HEIGHT 64    // OLED display height (in pixels)
-#define OLED_RESET -1    // Reset pin number (-1: sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C    // Display address; 0x3D for 128 X 64, 0x3C for 128 X 32
 
 // Instantiate an AHT20 sensor
 AHT20 aht20;
@@ -21,26 +17,15 @@ Adafruit_Sensor *bmp_temp = bmp280.getTemperatureSensor();
 Adafruit_Sensor *bmp_pressure = bmp280.getPressureSensor();
 
 // Instantiate a display
-// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-// Instantiate a display
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 // Instantiate an nRF24L01 transceiver
 RF24 radio(7, 8);   // Using pin 7 as the CE pin, and pin 8 as the CSN pin
 
+
 void setup(){
 
     Serial.begin(115200);
-
-    /*
-    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-        Serial.println(F("SSD1306 allocation failed"));
-        for(;;);    // Loop forever
-    }
-
-    display.clearDisplay();    // Clear buffer
-    */
 
     Wire.begin();
     aht20.begin();
@@ -88,25 +73,6 @@ void loop(){
     lcd.print(bmp280pres);
     delay(1000);
 }
-
-/*
-void printData(int hum, float temp) {
-    display.clearDisplay();
-    display.setTextSize(2);
-    display.setTextColor(SSD1306_WHITE);    // Draw white text
-    display.setCursor(0,0);    // Start at top left corner
-
-    display.print("Relative humidity: ");
-    display.print(hum);
-    display.print("%");
-    display.print("\n");
-    display.print("Temperature: ");
-    display.print(temp, 1);
-
-    display.display();
-    delay(400);
-}
-*/
 
 
 void receive(float temp, int hum, int pres){
