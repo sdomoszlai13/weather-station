3d74a49441561b01f38cb9835199565390c09cf0
sdomoszlai13 <sebastian.domoszlai@protonmail.com>
2023-03-19 19:54:14 +0100

    Added functionality to send data.

--- outdoor_sensor.ino
+++ outdoor_sensor.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <SPI.h>
 #include <Adafruit_SSD1306.h>
 #include <SparkFun_Qwiic_Humidity_AHT20.h>
 #include <Adafruit_BMP280.h>
@@ -10,55 +11,79 @@
 AHT20 aht20;
 
 // Instantiate a BMP280 sensor
-Adafruit_BMP280 bmp280; // Use I2C interface
+Adafruit_BMP280 bmp280;    // Use I2C interface
 Adafruit_Sensor *bmp_temp = bmp280.getTemperatureSensor();
 Adafruit_Sensor *bmp_pressure = bmp280.getPressureSensor();
 
 // Instantiate a display
-Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);    // Set display size
+
+// Instantiate a transceiver
+NRFLite _radio;
+const static uint8_t RADIO_ID = 1;    // This transceiver
+const static uint8_t DESTINATION_RADIO_ID = 0;    // Other transceiver
+const static uint8_t PIN_RADIO_CE = 9;
+const static uint8_t PIN_RADIO_CSN = 10;
+
+struct RadioPacket // Packet to be sent
+{
+    uint8_t temp;
+    uint32_t pres;
+    uint32_t hum;
+};
+
 
 void setup(){
     
     Serial.begin(115200);
     Wire.begin();
 
-    if (!aht20.begin())
-    {
+    // Setup AHT20 sensor
+    if (!aht20.begin()){
+
         Serial.println("Could not find a valid AHT20 sensor, check wiring"
                        " or try a different address!");
-        while (1)
-            delay(500);
-        Serial.println("Setup finished");
+        while (1) delay(500);
     }
 
-    if (!bmp280.begin())
-    {
+    Serial.println("Setup finished");
+
+    // Setup BMP280 sensor
+    if (!bmp280.begin()){
+
         Serial.println("Could not find a valid BMP280 sensor, check wiring"
                        " or try a different address!");
-        while (1)
-            delay(500);
-        Serial.println("Setup finished");
+        while (1) delay(500);
     }
 
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-    { // Address 0x3C for 128x32
-        Serial.println(("SSD1306 allocation failed"));
-        while (1)
-            delay(500);
-    }
+    Serial.println("Setup finished");
 
-    display.clearDisplay(); // Clear buffer
-    display.display();
-
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-
-    // Default settings from datasheet
     bmp280.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Normal mode
                        Adafruit_BMP280::SAMPLING_X2,     // Temperature oversampling
                        Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling
                        Adafruit_BMP280::FILTER_X16,      // Filtering
                        Adafruit_BMP280::STANDBY_MS_500); // Standby time
+
+    // Setup display
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)){ // Address 0x3C for 128x32
+
+        Serial.println(("SSD1306 allocation failed"));
+        while (1) delay(500);
+    }
+
+        display.clearDisplay();
+        display.display();
+        display.setTextSize(1);
+        display.setTextColor(WHITE);
+        Serial.println("Setup finished");
+    
+    //Setup transceiver
+    if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN)){
+        
+        Serial.println("Can't communicate with radio!");
+        while (1) delay();
+        Serial.println("Setup finished");
+    }
 }
 
 void loop(){
@@ -74,17 +99,17 @@ void loop(){
     delay(50);
 
     // Print values on display
-    Serial.println("Data: ");
-    Serial.println(aht20hum);
-    Serial.println(aht20temp);
     printData(aht20hum, aht20temp);
     delay(1000);
+
+    // Send values to base station
+    sendData(bmp280temp, bmp280pres, aht20hum);
 }
 
-void printData(int humidity, float temp)
-{
-    display.clearDisplay();
 
+void printData(int humidity, float temp){
+
+    display.clearDisplay();
     display.setTextSize(2);
     display.setTextColor(SSD1306_WHITE); // Draw white text
     display.setCursor(0, 0);             // Start at upper left corner
@@ -93,7 +118,30 @@ void printData(int humidity, float temp)
     display.print("\n");
     display.print(temp, 1);
     display.print(" C");
-
     display.display();
     delay(400);
+}
+
+
+void sendData(float temp, float pres, float hum){
+
+    static RadioPacket _radioData;
+    _radioData.temp = temp;
+    _radioData.pres = pres;
+    _radioData.hum = hum;
+
+    Serial.println("Sending...");
+    Serial.print(_radioData.temp);
+    Serial.print(_radioData.pres);
+    Serial.print(_radioData.hum);
+
+    if (_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData)))
+    {
+        Serial.println(" ...Success");
+    }
+    else
+    {
+        Serial.println(" ...Failed");
+
+    }
 }
